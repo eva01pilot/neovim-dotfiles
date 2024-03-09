4ec96156b4d7147e6b01ad3895494e66e6dc0a10
root <root@vm-536df0aa.na4u.ru>
2024-03-09 11:30:17 +0000

    prepush

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,7 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -28,18 +28,18 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
+  local opts = { buffer = bufnr, remap = false }
+  --client.resolved_capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -55,7 +55,7 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
 lsp.set_server_config({
   capabilities = {
@@ -67,20 +67,26 @@ lsp.set_server_config({
   }
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
 
-capabilities.textDocument.completion.completionItem.snippetSupport = false
 local nvim_lsp = require "lspconfig"
-  nvim_lsp.tailwindcss.setup {
+nvim_lsp.tailwindcss.setup {
   settings = {
     tailwindCSS = {
       experimental = {
         classRegex = {
-          {"clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)"}
+          { "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" }
         },
       },
     },
   },
+}
+require "cmp".setup {
+    sources = {
+        {
+            name = "nvim_lsp",
+            entry_filter = function(entry)
+                return require("cmp").lsp.CompletionItemKind.Snippet ~= entry:get_kind()
+            end
+        },
+    }
 }
