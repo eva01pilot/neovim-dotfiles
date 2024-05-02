145760debb1997037f2d449468e8b1105c1edb52
root <root@vm-9c5d19d9.na4u.ru>
2024-05-02 21:11:19 +0300

    fix k motion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -42,7 +42,7 @@ lsp.on_attach(function(client, bufnr)
   --client.resolved_capabilities.workspace.didchangewatchedfiles.dynamicregistration = true
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
-  vim.keymap.set("n", "k", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
